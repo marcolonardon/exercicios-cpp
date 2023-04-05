80ea3c39888fb49dbdce13bb7d4ffb76614f7499
Marco A. Lonardon Jr <112179645+marcolonardon@users.noreply.github.com>
2023-04-05 15:56:41 -0300

    1044

---
+++ beecrowd/iniciante/1044/1044.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a, b;
+main(){
+    scanf("%d %d",&a, &b);
+    if(a%b==0 || b%a==0){
+        printf("Sao Multiplos\n");
+    }else{
+        printf("Nao sao Multiplos\n");
+    }
+}
